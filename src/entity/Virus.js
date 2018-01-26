5953bf5ef5f9969990c52f3674aa9ca2ab6fdf51
DeadLord1337 <zaxarovcergay@mail.ru>
2018-01-26 16:29:59 +0300

    Update Virus.js

--- src/entity/Virus.js
+++ src/entity/Virus.js
@@ -38,17 +38,17 @@ Virus.prototype.onEaten = function(cell) {
     var config = this.gameServer.config;
 
     var cellsLeft = (config.virusMaxCells || config.playerMaxCells) - cell.owner.cells.length;
-    if (cellsLeft === 0) return;
+        if (cellsLeft <= 0) return;
     var splitMin = config.virusMaxPoppedSize * config.virusMaxPoppedSize / 100;
     var cellMass = cell._mass, splits = [], splitCount, splitMass;
 
     if (config.virusEqualPopSize) {
-        // definite monotone splits
+        // Definite monotone splits
         splitCount = Math.min(~~(cellMass / splitMin), cellsLeft);
         splitMass = cellMass / (1 + splitCount);
         for (var i = 0; i < splitCount; i++)
-            splits.push(splitMass);
-        return this.explodeCell(cell, splits);
+            splits.push (splitMass);
+        return this.explodeCell (cell, splits);
     }
 
     if (cellMass / cellsLeft < splitMin) {
@@ -57,38 +57,38 @@ Virus.prototype.onEaten = function(cell) {
         splitMass = cellMass / splitCount;
         while (splitMass > splitMin && splitCount * 2 < cellsLeft)
             splitMass = cellMass / (splitCount *= 2);
-        splitMass = cellMass / (splitCount + 1);
-        while (splitCount-- > 0) splits.push(splitMass);
-        return this.explodeCell(cell, splits);
+            splitMass = cellMass / (splitCount + 1);
+        while (splitCount-- > 0) splits.push (splitMass);
+            return this.explodeCell (cell, splits);
     }
 
-    // half-half splits
+    // Half-half splits
     var splitMass = cellMass / 2;
     var massLeft = cellMass / 2;
     while (cellsLeft-- > 0) {
         if (massLeft / cellsLeft < splitMin) {
             splitMass = massLeft / cellsLeft;
-            while (cellsLeft-- > 0) splits.push(splitMass);
+            while (cellsLeft-- > 0) splits.push (splitMass);
         }
         while (splitMass >= massLeft && cellsLeft > 0)
             splitMass /= 2;
-        splits.push(splitMass);
+        splits.push (splitMass);
         massLeft -= splitMass;
     }
-    this.explodeCell(cell, splits);
+    this.explodeCell (cell, splits);
 };
 
-Virus.prototype.explodeCell = function(cell, splits) {
+Virus.prototype.explodeCell = function (cell, splits) {
     for (var i = 0; i < splits.length; i++)
         this.gameServer.splitPlayerCell(cell.owner, cell, 2 * Math.PI * Math.random(), splits[i]);
 };
 
-Virus.prototype.onAdd = function(gameServer) {
-    gameServer.nodesVirus.push(this);
+Virus.prototype.onAdd = function (gameServer) {
+    gameServer.nodesVirus.push (this);
 };
 
-Virus.prototype.onRemove = function(gameServer) {
-    var index = gameServer.nodesVirus.indexOf(this);
+Virus.prototype.onRemove = function (gameServer) {
+    var index = gameServer.nodesVirus.indexOf (this);
     if (index != -1)
-        gameServer.nodesVirus.splice(index, 1);
+        gameServer.nodesVirus.splice (index, 1);
 };
